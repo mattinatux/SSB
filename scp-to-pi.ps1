72d32bceab5a76830bfe213199d08e60be114894
Matt <40341917+mattinatux@users.noreply.github.com>
2023-02-04 08:53:16 -0600

    Add files via upload

--- scp-to-pi.ps1
+++ scp-to-pi.ps1
@@ -1,8 +1,8 @@
 <# SCP step. Moved this out of icalbuddy.ps1 to address issues that stopped the cron.
 The If/Else addresses an issue with the client IP changing on the VPN. "It's only temporary unless it works." #>
 
-if (test-connection -computername 192.168.2.2 -count 1 -timetolive 128 -quiet) {
-    scp -q /Volumes/drivemecrazy/Tech/Scripts/SimpleScheduleBoard/index.html pi@192.168.2.2:/var/www/html/index.html
-} elseif (test-connection -computername 192.168.2.3 -count 1 -timetolive 128 -quiet) {
-    scp -q /Volumes/drivemecrazy/Tech/Scripts/SimpleScheduleBoard/index.html pi@192.168.2.3:/var/www/html/index.html
+if (test-connection -computername <IP_OR_NAME_1> -count 1 -timetolive 128 -quiet) {
+    scp -q /path/To/index.html pi@<IP_OR_NAME_1>:/var/www/html/index.html
+} elseif (test-connection -computername <IP_OR_NAME_2> -count 1 -timetolive 128 -quiet) {
+    scp -q /path/To/index.html pi@<IP_OR_NAME_2>:/var/www/html/index.html
 }
